0ec1f205efeeeef25093bb2105d4b120ab1603a8
Marie Cari <mcari@nyc.yearup.org>
2022-11-18 23:26:46 -0500

    step 2 completed

---
+++ Lectures/src/main/java/com/yearup/week2/homework3/MilkyWay.java
@@ -0,0 +1,14 @@
+package com.yearup.week2.homework3;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class MilkyWay {
+    static class MilkWay1 {
+
+    //1) Create a static class which has one method to display the text “Your planet belongs to
+    //Milky way galaxy.”
+        String displayMilkwayMessage() {
+            return "Your planet belongs to Milkway Way Galaxy";
+        }
+    }
+}
